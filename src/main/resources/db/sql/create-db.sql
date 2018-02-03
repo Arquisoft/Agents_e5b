0e39a881ddab42aabe9b1f5d3406cc13b955fca2
adrycarou896 <35782307+adrycarou896@users.noreply.github.com>
2018-02-03 13:47:30 +0100

    Update create-db.sql

--- src/main/resources/db/sql/create-db.sql
+++ src/main/resources/db/sql/create-db.sql
@@ -1,14 +1,28 @@
+DROP TABLE citizen IF EXISTS;
+DROP TABLE agent IF EXISTS;
+
+CREATE TABLE agent (
+  id BIGINT PRIMARY KEY,
+  identificador VARCHAR(50),--nombre de usuario y único
+  contrasena VARCHAR(30),
+  nombre VARCHAR(50),
+  email VARCHAR(50),
+  localizacion VARCHAR(50),
+  tipo VARCHAR(50),
+  tipoCodigo INT
+);
+
 --DROP TABLE citizen IF EXISTS;
 
-CREATE TABLE citizen (
-  id      BIGINT PRIMARY KEY,
-  contrasena VARCHAR(30),
-  nombreUsuario  VARCHAR(50),
-  dni VARCHAR(50),
-  nombre VARCHAR(50),
-  apellidos VARCHAR(50),
-  fechaNacimiento DATE,
-  email VARCHAR(50),
-  direccionPostal VARCHAR(50),
-  nacionalidad VARCHAR(50)
-);
+--CREATE TABLE citizen (
+--  id      BIGINT PRIMARY KEY,
+--  contrasena VARCHAR(30),
+--  nombreUsuario  VARCHAR(50),
+--  dni VARCHAR(50),
+--  nombre VARCHAR(50),
+--  apellidos VARCHAR(50),
+--  fechaNacimiento DATE,
+--  email VARCHAR(50),
+--  direccionPostal VARCHAR(50),
+--  nacionalidad VARCHAR(50)
+--);
