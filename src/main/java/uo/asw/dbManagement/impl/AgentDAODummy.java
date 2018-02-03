6fade23971ab1d2734811374f095a66b5e90207c
Adrian <UO246262@uniovi.es>
2018-02-03 13:41:59 +0100

    Cambiar clase Agent con los atributos propios de la nueva BBDD

--- src/main/java/uo/asw/dbManagement/impl/AgentDAODummy.java
+++ src/main/java/uo/asw/dbManagement/impl/AgentDAODummy.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import uo.asw.dbManagement.AgentDAO;
 import uo.asw.dbManagement.model.Agent;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -18,31 +17,33 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public class AgentDAODummy implements AgentDAO {
-    private static Agent dummyCitizen;
+    private static Agent dummyAgent;
     @PersistenceContext
     private EntityManager entityManager;
     
     static {
-        dummyCitizen = new Agent("pass", "dummy", "123456", "Clara", "Oswald", new Date(), "clara@tardis.co.uk", "The Hyperspace", "Inglesa");
+    	dummyAgent=new Agent("123456", "pass", "Clara Oswald", "clara@tardis.co.uk", "", "Person", 1);
+        //dummyCitizen = new Agent("pass", "dummy", "123456", "Clara", "Oswald", new Date(), "clara@tardis.co.uk", "The Hyperspace", "Inglesa");
     }
 
     @Override
     public Agent getAgent(String login, String password) {
     	@SuppressWarnings("unchecked")
 		List<Agent> citizen =  entityManager.createQuery(
-    	        "from Citizen where nombreUsuario = ?1 and contraseña = ?2")
+    	        /*"from Citizen where nombreUsuario = ?1 and contraseña = ?2"*/
+				"from Agent where nombreUsuario = ?1 and contraseña = ?2")
     	        .setParameter(1, login).setParameter(2, password)
     	        .getResultList();
     	if(citizen.isEmpty())
     		return null;
     	return citizen.get(0);
-       // return dummyCitizen;
+       // return dummyAgent;
     }
 
     @Override
     public Agent updateInfo(Agent toUpdate) {
     	entityManager.merge(toUpdate);
-        dummyCitizen = toUpdate;
-        return dummyCitizen;
+        dummyAgent = toUpdate;
+        return dummyAgent;
     }
 }
